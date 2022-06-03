6157655b3f1cc269664f1d7e3d24f651102e138c
신은정 <ejshin0213@naver.com>
2022-06-03 19:48:57 +0900

    2.Lib Configuration

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-class Block {
-  constructor(private data: string) {}
-  static hello() {
-    return "hi";
-  }
-}
+//lib에서 dom을 작성해주어서 document, localstorage, window가 자동완성되어 사용가능하다.
+document.querySelector;
+localStorage.setItem;
